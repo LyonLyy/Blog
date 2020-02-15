4ae978de53ffbeb113add0c7bc11cb2d182956ba
firmfeng <121696493@qq.com>
2020-02-15 20:00:16 +0800

    搜索界面的完善

---
+++ src/main/java/cn/coderzhx/mapper/IndexMapper.java
@@ -0,0 +1,29 @@
+package cn.coderzhx.mapper;
+
+import cn.coderzhx.entity.*;
+import cn.coderzhx.pojo.Visit;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ *
+ *
+ */
+@Mapper
+@Configuration
+public interface IndexMapper {
+    List<Menu> listMenu();
+    List<Menu> listAdminMenu();
+    Config listConfig();
+    List<Link> listLink();
+    List<VisitCount> listVisit();
+    //访问人数
+    Visit listTotal();
+
+    void addipCount(@Param("ipAddress") String ipAddress);
+    void addNewVisit(VisitCount visitCount);
+
+}
